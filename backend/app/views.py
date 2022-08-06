099ce591fe2630a426a902f9c389713726fcd8fb
Humayun Kabir <mhkmcp@yahoo.com>
2022-08-06 13:23:39 +0600

    added Inventory, InventoryGroup views

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Inventory, InventoryGroup
+from .serializers import InventorySerializer, InventoryGroupSerializer
+from core.custom_method import IsAuthenticatedCustom
+
+
+class InventoryView(ModelViewSet):
+    queryset = Inventory.objects.select_related('group', 'created_by')
+    serializer_class = InventorySerializer
+    permission_classes = (IsAuthenticatedCustom, )
+    
+    def create(self, request, *args, **kwargs):
+        request.data.update({"created_by_id":request.user.id})
+        return super().create(request, *args, **kwargs)
+    
+
+class InventoryGroupView(ModelViewSet):
+    queryset = InventoryGroup.objects.select_related('belongs_to', 'created_by').prefetch_related('inventories')
+    serializer_class = InventoryGroupSerializer
+    permission_classes = (IsAuthenticatedCustom, )
+    
+    def create(self, request, *args, **kwargs):
+        request.data.update({"created_by_id":request.user.id})
+        return super().create(request, *args, **kwargs)
