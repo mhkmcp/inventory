f0a60844a45153d8a3d54cd013b38c0b7118dd09
Humayun Kabir <mhkmcp@yahoo.com>
2022-08-06 13:49:01 +0600

    added pagination and Shop model

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,16 +1,44 @@
+from ast import keyword
+from curses import keyname
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Count
 
 from .models import Inventory, InventoryGroup
 from .serializers import InventorySerializer, InventoryGroupSerializer
 from core.custom_method import IsAuthenticatedCustom
+from core.utils import CustomPagination, get_query
 
 
 class InventoryView(ModelViewSet):
     queryset = Inventory.objects.select_related('group', 'created_by')
     serializer_class = InventorySerializer
     permission_classes = (IsAuthenticatedCustom, )
+    pagination_class = CustomPagination
+    
+    def get_queryset(self):
+        if self.request.method.lower() != "get":
+            return self.queryset
+        
+        data = self.request.query_params.dict()
+        data.pop("page", None)
+        keyword = data.pop("keyword", None)
+        
+        results = self.queryset(**data)
+        if keyword:
+            search_fields = (
+                'code',
+                'created_by__fullname',
+                'created_by__email',
+                'group__name',
+                'name',
+            )
+            query = get_query(keyword, search_fields)
+            results = results.filter(query)
+
+        return results 
+    
     
     def create(self, request, *args, **kwargs):
         request.data.update({"created_by_id":request.user.id})
@@ -21,6 +49,29 @@ class InventoryGroupView(ModelViewSet):
     queryset = InventoryGroup.objects.select_related('belongs_to', 'created_by').prefetch_related('inventories')
     serializer_class = InventoryGroupSerializer
     permission_classes = (IsAuthenticatedCustom, )
+    pagination_class = (CustomPagination, )
+    
+    def get_queryset(self):
+        if self.request.method.lower() != "get":
+            return self.queryset
+        
+        data = self.request.query_params.dict()
+        data.pop("page", None)
+        keyword = data.pop("keyword", None)
+        
+        results = self.queryset(**data)
+        if keyword:
+            search_fields = (
+                'created_by__fullname',
+                'created_by__email',
+                'name',
+            )
+            query = get_query(keyword, search_fields)
+            results = results.filter(query)
+        
+        return results.annotate(
+            total_items = Count('inventories')
+        )
     
     def create(self, request, *args, **kwargs):
         request.data.update({"created_by_id":request.user.id})
