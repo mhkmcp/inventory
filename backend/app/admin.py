2ac71f2200ae793a6753d63bb5a2eec462bbd996
Humayun Kabir <mhkmcp@yahoo.com>
2022-08-06 13:03:02 +0600

    added Inventory and InventoryGroup model

---
+++ backend/app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Inventory, InventoryGroup
+
+admin.site.register((Inventory, InventoryGroup))
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
     
     'user_control',
+    'app',
 ]
 
 MIDDLEWARE = [
